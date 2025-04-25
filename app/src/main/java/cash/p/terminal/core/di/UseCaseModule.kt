d04b230777f9d38594ec528dfe8ace3df7d34325
Oleg Leonov <pomni.o.nas@gmail.com>
2025-04-25 10:33:10 +0700

    Preparing sources for f-droid: Removed crashlytics, firebase and google services for

--- app/src/main/java/cash/p/terminal/core/di/UseCaseModule.kt
+++ app/src/main/java/cash/p/terminal/core/di/UseCaseModule.kt
@@ -3,7 +3,6 @@ package cash.p.terminal.core.di
 import cash.p.terminal.core.usecase.CreateHardwareWalletUseCase
 import cash.p.terminal.core.usecase.UpdateChangeNowStatusesUseCase
 import cash.p.terminal.tangem.domain.usecase.ICreateHardwareWalletUseCase
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -11,6 +10,4 @@ import org.koin.dsl.module
 val useCaseModule = module {
     factoryOf(::UpdateChangeNowStatusesUseCase)
     factoryOf(::CreateHardwareWalletUseCase) bind ICreateHardwareWalletUseCase::class
-
-    factory { AppUpdateManagerFactory.create(get()) }
 }
