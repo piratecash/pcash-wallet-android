9cec8a82976f2ca67caec008902fdcd26e0af0db
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2025-07-09 08:02:58 +0300

    Preparing sources for f-droid: Removed crashlytics, firebase and google services for

--- app/src/main/java/cash/p/terminal/core/di/UseCaseModule.kt
+++ app/src/main/java/cash/p/terminal/core/di/UseCaseModule.kt
@@ -1,6 +1,5 @@
 package cash.p.terminal.core.di
 
-import cash.p.terminal.core.usecase.CheckGooglePlayUpdateUseCase
 import cash.p.terminal.core.usecase.CreateHardwareWalletUseCase
 import cash.p.terminal.core.usecase.GenerateMoneroWalletUseCase
 import cash.p.terminal.core.usecase.MoneroWalletUseCase
@@ -9,7 +8,6 @@ import cash.p.terminal.core.usecase.ValidateMoneroHeightUseCase
 import cash.p.terminal.core.usecase.ValidateMoneroMnemonicUseCase
 import cash.p.terminal.domain.usecase.GetReleaseNotesUseCase
 import cash.p.terminal.tangem.domain.usecase.ICreateHardwareWalletUseCase
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -19,10 +17,7 @@ val useCaseModule = module {
     factoryOf(::ValidateMoneroMnemonicUseCase)
     factoryOf(::ValidateMoneroHeightUseCase)
     factoryOf(::GetReleaseNotesUseCase)
-    factoryOf(::CheckGooglePlayUpdateUseCase)
     factoryOf(::MoneroWalletUseCase)
     factoryOf(::GenerateMoneroWalletUseCase)
     factoryOf(::CreateHardwareWalletUseCase) bind ICreateHardwareWalletUseCase::class
-
-    factory { AppUpdateManagerFactory.create(get()) }
 }
