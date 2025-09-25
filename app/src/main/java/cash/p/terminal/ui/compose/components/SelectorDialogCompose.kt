f8bd892abe57fccdc50fb14e2b336395bb8828c3
Oleg Leonov <pomni.o.nas@gmail.com>
2025-09-25 10:06:10 +0700

    Implement vertical scrolling for item selection dialog to fix problem with choosing wallet on ton connect screen

--- app/src/main/java/cash/p/terminal/ui/compose/components/SelectorDialogCompose.kt
+++ app/src/main/java/cash/p/terminal/ui/compose/components/SelectorDialogCompose.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import cash.p.terminal.ui_compose.components.VSpacer
@@ -53,48 +56,69 @@ fun <T> SelectorDialogCompose(
                         textAlign = TextAlign.Center
                     )
                 }
+                Divider(thickness = 1.dp, color = ComposeAppTheme.colors.steel10)
             }
-
-            items.forEach { item ->
-                Column(
-                    modifier = Modifier
-                        .clickable(
-                            enabled = !item.selected,
-                            onClick = {
-                                onSelectItem.invoke(item.item)
-                                onDismissRequest.invoke()
-                            }
-                        )
-                ) {
-                    Divider(thickness = 1.dp, color = ComposeAppTheme.colors.steel10)
-                    val color = if (item.selected) {
-                        cash.p.terminal.ui_compose.theme.ComposeAppTheme.colors.jacob
-                    } else {
-                        cash.p.terminal.ui_compose.theme.ComposeAppTheme.colors.leah
-                    }
-                    VSpacer(12.dp)
-                    Text(
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                items.forEachIndexed { index, item ->
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp),
-                        text = item.title,
-                        style = ComposeAppTheme.typography.body,
-                        color = color,
-                        textAlign = TextAlign.Center
-                    )
-                    item.subtitle?.let { subtitle ->
-                        VSpacer(1.dp)
-                        subhead2_grey(
+                            .clickable(
+                                enabled = !item.selected,
+                                onClick = {
+                                    onSelectItem.invoke(item.item)
+                                    onDismissRequest.invoke()
+                                }
+                            )
+
+                    ) {
+                        if (index != 0 || title != null) {
+                            Divider(thickness = 1.dp, color = ComposeAppTheme.colors.steel10)
+                        }
+                        val color = if (item.selected) {
+                            ComposeAppTheme.colors.jacob
+                        } else {
+                            ComposeAppTheme.colors.leah
+                        }
+                        VSpacer(12.dp)
+                        Text(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(horizontal = 16.dp),
-                            text = subtitle,
+                            text = item.title,
+                            style = ComposeAppTheme.typography.body,
+                            color = color,
                             textAlign = TextAlign.Center
                         )
+                        item.subtitle?.let { subtitle ->
+                            VSpacer(1.dp)
+                            subhead2_grey(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 16.dp),
+                                text = subtitle,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        VSpacer(12.dp)
                     }
-                    VSpacer(12.dp)
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SelectorDialogPreview() {
+    val items = List(30) {
+        SelectorItem("Item $it", it == 1, it)
+    }
+    ComposeAppTheme {
+        SelectorDialogCompose(
+            title = "Title",
+            items = items,
+            onDismissRequest = {},
+            onSelectItem = {}
+        )
     }
 }
