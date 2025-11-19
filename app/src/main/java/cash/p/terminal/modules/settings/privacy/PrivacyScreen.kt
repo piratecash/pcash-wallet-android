1d267eb16743e46a3035c3bb809a7ea71b6f0364
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2025-11-19 20:08:56 +0200

    Preparing sources for f-droid: Removed crashlytics, firebase and google services for

--- app/src/main/java/cash/p/terminal/modules/settings/privacy/PrivacyScreen.kt
+++ app/src/main/java/cash/p/terminal/modules/settings/privacy/PrivacyScreen.kt
@@ -39,8 +39,7 @@ import cash.p.terminal.ui_compose.components.SectionUniversalLawrence
 @Composable
 fun PrivacyScreen(
     navController: NavController,
-    uiState: PrivacyUiState,
-    toggleCrashData: (Boolean) -> Unit
+    uiState: PrivacyUiState
 ) {
 
     Column(
@@ -67,23 +66,6 @@ fun PrivacyScreen(
             BulletedText(R.string.Privacy_BulletedText2)
             BulletedText(R.string.Privacy_BulletedText3)
             VSpacer(height = 16.dp)
-
-            SectionUniversalLawrence {
-                CellUniversal {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_share_24px),
-                        contentDescription = "Share",
-                        tint = ComposeAppTheme.colors.grey
-                    )
-                    HSpacer(width = 16.dp)
-                    body_leah(text = stringResource(R.string.ShareCrashData))
-                    HFillSpacer(minWidth = 8.dp)
-                    HsSwitch(
-                        checked = uiState.uiStatsEnabled,
-                        onCheckedChange = toggleCrashData
-                    )
-                }
-            }
         }
 
         Divider(
