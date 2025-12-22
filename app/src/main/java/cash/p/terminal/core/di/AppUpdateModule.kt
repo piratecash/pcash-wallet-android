e6ce5fa02d200c6bbd61e5c1a6835014a05043c5
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2025-12-22 20:47:10 +0200

    Preparing sources for f-droid: Removed crashlytics, firebase and google services for

--- app/src/main/java/cash/p/terminal/core/di/AppUpdateModule.kt
+++
@@ -1,8 +0,0 @@
-package cash.p.terminal.core.di
-
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import org.koin.dsl.module
-
-val appUpdateModule = module {
-    factory { AppUpdateManagerFactory.create(get()) }
-}
