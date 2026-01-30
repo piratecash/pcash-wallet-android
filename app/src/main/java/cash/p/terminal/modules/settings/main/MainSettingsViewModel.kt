175c7f09b6e09444a85b22117cfb72ce3dc55ca3
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-01-30 10:11:15 +0200

    Removed rate app dialog for fdroid

--- app/src/main/java/cash/p/terminal/modules/settings/main/MainSettingsViewModel.kt
+++ app/src/main/java/cash/p/terminal/modules/settings/main/MainSettingsViewModel.kt
@@ -86,7 +86,6 @@ class MainSettingsViewModel(
     val currentAccountSupportsTonConnect: Boolean
         get() = accountManager.activeAccount?.supportsTonConnect() == true
 
-
     private var wcCounterType: CounterType? = null
     private var wcSessionsCount = walletConnectSessionCount
     private var wcPendingRequestCount = 0
