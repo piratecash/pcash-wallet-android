776742dd8d9d0d53f7d96fb13fca5f4d34d74d7a
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-03-01 12:03:37 +0200

    Preparing sources for f-droid: Removed crashlytics, firebase and google services for

--- app/src/main/java/cash/p/terminal/modules/settings/about/AboutFragment.kt
+++ app/src/main/java/cash/p/terminal/modules/settings/about/AboutFragment.kt
@@ -95,7 +95,6 @@ private fun AboutNavHost(fragmentNavController: NavController) {
             PrivacyScreen(
                 navController = navController,
                 uiState = viewModel.uiState,
-                toggleCrashData = viewModel::toggleCrashData
             )
         }
         composablePopup(TermsPage) { TermsScreen(navController) }
