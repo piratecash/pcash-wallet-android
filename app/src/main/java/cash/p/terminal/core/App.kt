fc2728951f0c6971762d47c1815d63edaca007f0
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-03-01 12:03:37 +0200

    Preparing sources for f-droid: Removed crashlytics, firebase and google services for

--- app/src/main/java/cash/p/terminal/core/App.kt
+++ app/src/main/java/cash/p/terminal/core/App.kt
@@ -82,7 +82,6 @@ import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import coil3.request.crossfade
 import coil3.svg.SvgDecoder
 import com.getkeepsafe.relinker.ReLinker
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.m2049r.levin.util.NetCipherHelper
 import com.m2049r.levin.util.NetCipherHelper.OnStatusChangedListener
 import com.m2049r.xmrwallet.model.WalletManager
@@ -257,15 +256,6 @@ class App : CoreApp(), WorkConfiguration.Provider, SingletonImageLoader.Factory 
         }
 
         RxJavaPlugins.setErrorHandler { e: Throwable? ->
-            Log.w("RxJava ErrorHandler", e)
-            e?.let {
-                if (localStorage.shareCrashDataEnabled) {
-                    FirebaseCrashlytics.getInstance().recordException(e)
-                }
-            }
-        }
-
-        RxJavaPlugins.setErrorHandler { e: Throwable? ->
             Timber.tag("RxJava ErrorHandler").e(e ?: return@setErrorHandler)
         }
 
@@ -294,9 +284,6 @@ class App : CoreApp(), WorkConfiguration.Provider, SingletonImageLoader.Factory 
         balanceViewTypeManager = BalanceViewTypeManager(localStorage)
 
         startTasks()
-
-        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled =
-            localStorage.shareCrashDataEnabled
     }
 
     /**
