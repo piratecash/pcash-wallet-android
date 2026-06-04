a29444ba0c18a1e77670c84071fd689db69fe79d
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2026-06-04 14:18:47 +0200

    Preparing sources for f-droid: Removed crashlytics, firebase and google services for

--- app/src/main/java/cash/p/terminal/modules/settings/privacy/PrivacyViewModel.kt
+++ app/src/main/java/cash/p/terminal/modules/settings/privacy/PrivacyViewModel.kt
@@ -1,7 +1,6 @@
 package cash.p.terminal.modules.settings.privacy
 
 import cash.p.terminal.core.ILocalStorage
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import io.horizontalsystems.core.ViewModelUiState
 
 class PrivacyViewModel(private val localStorage: ILocalStorage) :
@@ -9,12 +8,6 @@ class PrivacyViewModel(private val localStorage: ILocalStorage) :
     override fun createState() = PrivacyUiState(
         uiStatsEnabled = localStorage.shareCrashDataEnabled
     )
-
-    fun toggleCrashData(enabled: Boolean) {
-        localStorage.shareCrashDataEnabled = enabled
-        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = enabled
-        emitState()
-    }
 }
 
 data class PrivacyUiState(val uiStatsEnabled: Boolean)
