5770146ff4c360e55d0d7e89cf0a5b723762a871
Oleg Leonov <pomni.o.nas@gmail.com>
2026-07-10 21:47:24 +0300

    Update test configurations and dependencies for improved performance

--- app/src/test/java/cash/p/terminal/core/storage/OfflineSignedTransactionMigrationTest.kt
+++ app/src/test/java/cash/p/terminal/core/storage/OfflineSignedTransactionMigrationTest.kt
@@ -22,7 +22,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(manifest = Config.NONE, sdk = [34])
+@Config(manifest = Config.NONE)
 class OfflineSignedTransactionMigrationTest {
 
     private lateinit var helper: SupportSQLiteOpenHelper
