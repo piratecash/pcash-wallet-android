b2d212f185ce56dbbee1c7ebb0c390417f2c4438
Oleg Leonov <pomni.o.nas@gmail.com>
2026-07-31 14:43:34 +0300

    Add offline sign button

--- core/ui-compose/src/main/java/cash/p/terminal/ui_compose/components/HsSettingCell.kt
+++ core/ui-compose/src/main/java/cash/p/terminal/ui_compose/components/HsSettingCell.kt
@@ -2,11 +2,11 @@ package cash.p.terminal.ui_compose.components
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,6 +26,9 @@ fun HsSettingCell(
     onClick: (() -> Unit)? = null,
     enabled: Boolean = true,
 ) {
+    val primaryColor = if (enabled) ComposeAppTheme.colors.leah else ComposeAppTheme.colors.grey50
+    val secondaryColor = if (enabled) ComposeAppTheme.colors.grey else ComposeAppTheme.colors.grey50
+
     RowUniversal(
         modifier = Modifier.padding(horizontal = 16.dp),
         onClick = onClick,
@@ -36,32 +39,37 @@ fun HsSettingCell(
                 modifier = Modifier.size(24.dp),
                 painter = painterResource(id = icon),
                 contentDescription = null,
-                tint = iconTint ?: ComposeAppTheme.colors.grey
+                tint = if (enabled) iconTint ?: secondaryColor else secondaryColor,
             )
         }
-        body_leah(
+        Text(
             text = stringResource(title),
+            color = primaryColor,
+            style = ComposeAppTheme.typography.body,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
-            modifier = Modifier.padding(start = if (icon != null) 16.dp else 0.dp, end = 16.dp)
+            modifier = Modifier.padding(start = if (icon != null) 16.dp else 0.dp, end = 16.dp),
         )
         Spacer(Modifier.weight(1f))
 
         if (value != null) {
-            subhead1_grey(
+            Text(
                 text = value,
+                color = secondaryColor,
+                style = ComposeAppTheme.typography.subhead1,
                 maxLines = 1,
                 modifier = Modifier.padding(
                     horizontal = if (onClick != null) 8.dp else 0.dp
-                )
+                ),
             )
         }
 
         if (onClick != null) {
-            Image(
+            Icon(
                 modifier = Modifier.size(20.dp),
                 painter = painterResource(id = R.drawable.ic_arrow_right),
                 contentDescription = null,
+                tint = secondaryColor,
             )
         }
     }
